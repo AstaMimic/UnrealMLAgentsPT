3e807ab6c86e238e08a2f7334597a0873ecc13f6
Alec Otto <140107635+AstaMimic@users.noreply.github.com>
2025-08-15 20:40:40 -0500

    Refactor training editor engine header

--- Source/SimCadenceController/Public/TrainingEditorEngine.h
+++ Source/SimCadenceController/Public/TrainingEditorEngine.h
@@ -4,23 +4,24 @@
 
 #if WITH_SIMCADENCE_TRAINING_ENGINE
 	#include "Editor/EditorEngine.h"
-	#include "TrainingEditorEngine.generated.h"
-
-UCLASS(config = Engine)
-class SIMCADENCECONTROLLER_API UTrainingEditorEngine : public UEditorEngine
-{
-	GENERATED_BODY()
-
-protected:
-	virtual void RedrawViewports(bool bShouldPresent) override;
-};
 #else
 	#include "Engine/Engine.h"
-	#include "TrainingEditorEngine.generated.h"
+#endif
+
+#include "TrainingEditorEngine.generated.h"
 
 UCLASS(config = Engine)
-class SIMCADENCECONTROLLER_API UTrainingEditorEngine : public UEngine
+class SIMCADENCECONTROLLER_API UTrainingEditorEngine
+#if WITH_SIMCADENCE_TRAINING_ENGINE
+	: public UEditorEngine
+#else
+	: public UEngine
+#endif
 {
 	GENERATED_BODY()
-};
+
+#if WITH_SIMCADENCE_TRAINING_ENGINE
+protected:
+	virtual void RedrawViewports(bool bShouldPresent) override;
 #endif
+};
