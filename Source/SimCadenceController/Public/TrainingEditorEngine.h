478c21eb11c0402fb1fd7d27e615822b66d4c709
Alec Otto <140107635+AstaMimic@users.noreply.github.com>
2025-08-15 20:57:08 -0500

    refactor: unify training engine base class alias

--- Source/SimCadenceController/Public/TrainingEditorEngine.h
+++ Source/SimCadenceController/Public/TrainingEditorEngine.h
@@ -4,23 +4,21 @@
 
 #if WITH_SIMCADENCE_TRAINING_ENGINE
 	#include "Editor/EditorEngine.h"
-	#include "TrainingEditorEngine.generated.h"
-
-UCLASS(config = Engine)
-class SIMCADENCECONTROLLER_API UTrainingEditorEngine : public UEditorEngine
-{
-	GENERATED_BODY()
-
-protected:
-	virtual void RedrawViewports(bool bShouldPresent) override;
-};
+using FTrainingEditorEngineSuper = UEditorEngine;
 #else
 	#include "Engine/Engine.h"
-	#include "TrainingEditorEngine.generated.h"
+using FTrainingEditorEngineSuper = UEngine;
+#endif
+
+#include "TrainingEditorEngine.generated.h"
 
 UCLASS(config = Engine)
-class SIMCADENCECONTROLLER_API UTrainingEditorEngine : public UEngine
+class SIMCADENCECONTROLLER_API UTrainingEditorEngine : public FTrainingEditorEngineSuper
 {
 	GENERATED_BODY()
-};
+
+#if WITH_SIMCADENCE_TRAINING_ENGINE
+protected:
+	virtual void RedrawViewports(bool bShouldPresent) override;
 #endif
+};
